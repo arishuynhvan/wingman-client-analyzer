6f1e9cadb3b2d569755cec66cb1d8f522a541941
arishuynhvan <arishuynhvan@gmail.com>
2016-12-09 18:09:58 +0800

    Setup UI for importing log file & webpack for multiple js entry points

---
+++ dist/JS/parseLog.js
@@ -0,0 +1,6 @@
+/** To retrieve file from UI and parse it for JSON objects */
+/** TODO:
+  * Retrieve file from UI
+  * Find and store JSON strings of client states & appcast
+  * Convert right JSON strings to JSON objects
+  * Export these JSON objects */
